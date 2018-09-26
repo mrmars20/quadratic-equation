994cea1cb11111b32d2e4950b7ba80a7706a50ea
mrmars20 <43221807+mrmars20@users.noreply.github.com>
2018-09-26 17:19:24 +0300

    task4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  var arr = equation.split(' '),
+  a = arr[0],
+  b = arr[4],
+  c = arr[8],
+  znB = arr[3],
+  znC = arr[7];
+
+  
+  if(znB === "-"){
+    b = b*(-1);
+  }
+  if(znC === "-"){
+    c = c*(-1);
+  }
+ 
+  var D = Math.pow(b,2) - 4*a*c,
+  x1 = Math.round((-b + Math.sqrt(D))/(2*a)),
+  x2 = Math.round((-b - Math.sqrt(D))/(2*a));
+
+  if(x1 > x2) {
+    return [x2, x1];
+  }
+    return [x1,x2];
+} 
